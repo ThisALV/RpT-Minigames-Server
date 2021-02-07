fd7ff91c855281dd7384ab3bb18db76232ca8ad2
ThisALV <lel.charriere@orange.fr>
2021-02-07 23:14:23 +0100

    Add source configuration file Config.hpp

--- rpt-server/Main.cpp
+++ rpt-server/Main.cpp
@@ -1,3 +1,8 @@
+#include <iostream>
+#include <Rpt-Config/Config.hpp>
+
 int main(const int argc, const char* argv[]) {
+    std::cout << "Running RpT server " << RPT_VERSION << "." << std::endl;
+
     return 0;
 }
