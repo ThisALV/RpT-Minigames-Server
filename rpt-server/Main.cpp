21f70b5867a7d72fd355dffe05da29a093ae0f47
ThisALV <lel.charriere@orange.fr>
2021-02-08 14:53:45 +0100

    Improved runtime platform name deduction for main()

--- rpt-server/Main.cpp
+++ rpt-server/Main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <Rpt-Config/Config.hpp>
 
-#define str(s) #s
-#define xstr(s) str(s)
-#define RPT_RUNTIME_PLATFORM_NAME xstr(RPT_RUNTIME_PLATFORM)
+#if RPT_RUNTIME_PLATFORM == Unix
+#define RPT_RUNTIME_PLATFORM_NAME "Unix"
+#else
+#define RPT_RUNTIME_PLATFORM_NAME "Win32"
+#endif
 
 int main(const int argc, const char* argv[]) {
     std::cout << "Running RpT server " RPT_VERSION " on " RPT_RUNTIME_PLATFORM_NAME "." << std::endl;
