e25415fcac8332d1ca526b377939348fb8b26741
ThisALV <lel.charriere@orange.fr>
2021-02-08 11:22:54 +0100

    Add platform config constant

--- Config.hpp
+++ Config.hpp
@@ -6,11 +6,17 @@
 namespace RpT {
 namespace Config {
 
+enum struct Platform {
+    Win32, Unix
+};
+
 constexpr std::size_t VERION_MAJOR { @PROJECT_VERSION_MAJOR@ };
 constexpr std::size_t VERSION_MINOR { @PROJECT_VERSION_MINOR@ };
 constexpr std::size_t VERSION_PATCH { @PROJECT_VERSION_PATCH@ };
 
 constexpr std::string_view VERSION { "@PROJECT_VERSION@" };
+
+constexpr Platform platform { Platform::@RPT_TARGET_PLATFORM@ };
 
 }
 }
