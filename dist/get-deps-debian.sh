cf566ddc8e3e8df08f196c031b0b34d59c88215f
ThisALV <lel.charriere@orange.fr>
2021-02-12 20:11:27 +0100

    Fix: use local vars for shell functions

--- dist/get-deps-debian.sh
+++ dist/get-deps-debian.sh
@@ -2,7 +2,7 @@
 
 
 function echoPackageInstall() {
-  name="$1"
+  local name="$1"
 
   echo
   echo "============================================================"
@@ -12,24 +12,32 @@ function echoPackageInstall() {
 }
 
 function extractGitHubSource() {
-  author="$1"
-  name="$2"
-  branch_name="$3"
-  extract_dir="$4"
+  local author="$1"
+  local name="$2"
+  local branch_name="$3"
+  local extract_dir="$4"
 
-  download_dir="download/$author"
-  dest_dir="$extract_dir/$author"
-  archive="$name-$branch_name.tar.gz"
+  local dep_full_name="$author/$name"
+  local download_dir="download/$author"
+  local dest_dir="$extract_dir/$author"
+  local archive="$name-$branch_name.tar.gz"
+  local sources_url="https://github.com/$author/$name/archive/$branch_name.tar.gz"
 
+  echo "Create download directory $download_dir..." && \
   mkdir -p "$download_dir" && \
-  wget -O "$download_dir/$archive" "https://github.com/$author/$name/archive/$branch_name.tar.gz" && \
+  echo "Download sources from $sources_url..." && \
+  wget -O "$download_dir/$archive" "$sources_url" && \
+  echo "Create destination directory $dest_dir..." && \
   mkdir -p "$dest_dir" && \
-  tar -xzf "$download_dir/$archive" -C "$dest_dir"
+  echo "Extract downloaded sources to $dest_dir..." && \
+  tar -xzf "$download_dir/$archive" -C "$dest_dir" && \
+  echo "Extracting done for $dep_full_name." || \
+  echo "Error : unable to extract sources for $dep_full_name."
 }
 
 
 function tryAptGet() {
-  name="$1"
+  local name="$1"
 
   echoPackageInstall "$name"
 
@@ -41,21 +49,27 @@ function tryAptGet() {
 }
 
 function tryHeaderOnlyGet() {
-  author="$1"
-  name="$2"
-  branch_name="$3"
+  local author="$1"
+  local name="$2"
+  local branch_name="$3"
 
-  dep_full_name="$author/$name"
+  local dep_full_name="$author/$name"
   echoPackageInstall "$dep_full_name"
 
-  caller_dir="$(pwd)" # Must be absolute for working-directory restore
-  install_dir="$caller_dir/install" # Must be absolute for CMake configuration
-  deps_dir="build"
+  local caller_dir="$(pwd)" # Must be absolute for working-directory restore
+  local install_dir="$caller_dir/install" # Must be absolute for CMake configuration
+  local deps_dir="build"
+  local project_dir="$deps_dir/$author/$name-$branch_name"
 
+  echo "Create deps build directory at $deps_dir..." && \
   mkdir -p $deps_dir && \
+  echo "Extract $dep_full_name sources..." && \
   extractGitHubSource "$author" "$name" "$branch_name" "$deps_dir" && \
-  cd "$deps_dir/$author/$name-$branch_name" && \
+  echo "Go to $dep_full_name project directory $project_dir..." && \
+  cd "$project_dir" && \
+  echo "Configure project..." && \
   cmake "-DCMAKE_INSTALL_PREFIX=$install_dir" . && \
+  echo "Install project to $install_dir..." && \
   cmake --install . && \
   echo "Successfully get $dep_full_name." || \
   echo "Error : unable to get $dep_full_name."
@@ -64,17 +78,18 @@ function tryHeaderOnlyGet() {
 }
 
 function trySourceGet() {
-  author="$1"
-  name="$2"
-  branch_name="$3"
-  special_build_dir="$4"
+  local author="$1"
+  local name="$2"
+  local branch_name="$3"
+  local special_build_dir="$4"
 
-  dep_full_name="$author/$name"
+  local dep_full_name="$author/$name"
   echoPackageInstall "$dep_full_name"
 
-  caller_dir="$(pwd)"
-  install_dir="$caller_dir/install"
-  deps_dir="build"
+  local caller_dir="$(pwd)"
+  local install_dir="$caller_dir/install"
+  local deps_dir="build"
+  local project_dir="$deps_dir/$author/$name-$branch_name"
 
   if [ "$special_build_dir" ]; then
     build_dir="$deps_dir/$author/$special_build_dir/build"
@@ -82,18 +97,25 @@ function trySourceGet() {
     build_dir="$deps_dir/$author/$name-$branch_name/build"
   fi
 
+  echo "Create deps build directory at $deps_dir..." && \
   mkdir -p "$deps_dir" && \
   extractGitHubSource "$author" "$name" "$branch_name" "$deps_dir"
+  echo "Create $dep_full_name build directory at $build_dir..."
   mkdir -p "$build_dir" && \
+  echo "Go to $dep_full_name project directory $project_dir..." && \
   cd "$build_dir" && \
+  echo "Configure project..." && \
   cmake "-DCMAKE_INSTALL_PREFIX=$install_dir" "-DCMAKE_AR=$(which "$AR")" "-DCMAKE_RANLIB=$(which "$RANLIB")" .. && \
+  echo "Build project..." && \
   cmake --build . -- "-j$(nproc)" && \
+  echo "Install project to $install_dir..." && \
   cmake --install . && \
   echo "Successfully get $dep_full_name." || \
   echo "Error : unable to get $dep_full_name."
 
   cd "$caller_dir" || exit 1
 }
+
 
 if [ ! "$AR" ]; then
   AR="ar"
