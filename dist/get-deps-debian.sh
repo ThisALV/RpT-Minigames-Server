0cf9f807595ce21ee5a08ca524bfed8d7895546a
ThisALV <lel.charriere@orange.fr>
2021-02-12 19:26:04 +0100

    Add quotes on "unsafe" variables

--- dist/get-deps-debian.sh
+++ dist/get-deps-debian.sh
@@ -14,15 +14,15 @@ function echoPackageInstall() {
 function extractGitHubSource() {
   author="$1"
   name="$2"
-  branch="$3"
+  branch_name="$3"
   extract_dir="$4"
 
   download_dir="download/$author"
   dest_dir="$extract_dir/$author"
-  archive="$name-$branch.tar.gz"
+  archive="$name-$branch_name.tar.gz"
 
   mkdir -p "$download_dir" && \
-  wget -O "$download_dir/$archive" "https://github.com/$author/$name/archive/$branch.tar.gz" && \
+  wget -O "$download_dir/$archive" "https://github.com/$author/$name/archive/$branch_name.tar.gz" && \
   mkdir -p "$dest_dir" && \
   tar -xzf "$download_dir/$archive" -C "$dest_dir"
 }
@@ -31,9 +31,9 @@ function extractGitHubSource() {
 function tryAptGet() {
   name="$1"
 
-  echoPackageInstall $name
+  echoPackageInstall "$name"
 
-  if apt-get install -y $name; then
+  if apt-get install -y "$name"; then
     echo "Successfully get $name."
   else
     echo "Error : unable to get $name." >&2
@@ -43,50 +43,50 @@ function tryAptGet() {
 function tryHeaderOnlyGet() {
   author="$1"
   name="$2"
-  branch="$3"
+  branch_name="$3"
 
   dep_full_name="$author/$name"
-  echoPackageInstall $dep_full_name
+  echoPackageInstall "$dep_full_name"
 
-  caller_dir="$(pwd)"
-  install_dir="$caller_dir/install"
+  caller_dir="$(pwd)" # Must be absolute for working-directory restore
+  install_dir="$caller_dir/install" # Must be absolute for CMake configuration
   deps_dir="build"
 
   mkdir -p $deps_dir && \
-  extractGitHubSource "$author" "$name" "$branch" "$deps_dir" && \
-  cd "$deps_dir/$author/$name-$branch" && \
+  extractGitHubSource "$author" "$name" "$branch_name" "$deps_dir" && \
+  cd "$deps_dir/$author/$name-$branch_name" && \
   cmake "-DCMAKE_INSTALL_PREFIX=$install_dir" . && \
   cmake --install . && \
   echo "Successfully get $dep_full_name." || \
   echo "Error : unable to get $dep_full_name."
 
-  cd $caller_dir || exit 1
+  cd "$caller_dir" || exit 1
 }
 
 function trySourceGet() {
   author="$1"
   name="$2"
-  branch="$3"
+  branch_name="$3"
 
   dep_full_name="$author/$name"
-  echoPackageInstall $dep_full_name
+  echoPackageInstall "$dep_full_name"
 
   caller_dir="$(pwd)"
   install_dir="$caller_dir/install"
   deps_dir="build"
-  build_dir="$deps_dir/$author/$name-$branch/build"
+  build_dir="$deps_dir/$author/$name-$branch_name/build"
 
-  mkdir -p $deps_dir && \
-  extractGitHubSource "$author" "$name" "$branch" "$deps_dir"
-  mkdir -p $build_dir && \
-  cd $build_dir && \
+  mkdir -p "$deps_dir" && \
+  extractGitHubSource "$author" "$name" "$branch_name" "$deps_dir"
+  mkdir -p "$build_dir" && \
+  cd "$build_dir" && \
   cmake "-DCMAKE_INSTALL_PREFIX=$install_dir" "-DCMAKE_AR=$(which "$AR")" "-DCMAKE_RANLIB=$(which "$RANLIB")" .. && \
   cmake --build . -- "-j$(nproc)" && \
   cmake --install . && \
   echo "Successfully get $dep_full_name." || \
   echo "Error : unable to get $dep_full_name."
 
-  cd $caller_dir || exit 1
+  cd "$caller_dir" || exit 1
 }
 
 
@@ -94,4 +94,4 @@ tryAptGet libboost-all-dev
 tryAptGet liblua5.3-dev
 tryHeaderOnlyGet nlohmann json master
 tryHeaderOnlyGet ThePhD sol2 main
-trySourceGet gabime spdlog "v1.x"
+trySourceGet gabime spdlog v1.x
