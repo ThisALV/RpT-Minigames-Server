64c093e77c1c0ba1daf693fe44dbc85cbc5c4a07
ThisALV <lel.charriere@orange.fr>
2021-02-13 19:29:18 +0100

    Fix: missing headers file into API doc

--- rpt-core/include/RpT-Core/NetworkProtocol.hpp
+++ rpt-core/include/RpT-Core/NetworkProtocol.hpp
@@ -1,8 +1,11 @@
 #ifndef RPTOGETHER_SERVER_NETWORKPROTOCOL_HPP
 #define RPTOGETHER_SERVER_NETWORKPROTOCOL_HPP
 
+/**
+ * @brief Network services handling
+ */
 class NetworkProtocol {
 
 };
 
-#endif //RPTOGETHER_SERVER_NETWORKPROTOCOL_HPP
+#endif // RPTOGETHER_SERVER_NETWORKPROTOCOL_HPP
