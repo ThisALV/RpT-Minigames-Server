8f98b95b064c610af4ef6ee92b6e5dfa9cd40b8c
ThisALV <lel.charriere@orange.fr>
2021-02-17 11:26:06 +0100

    Fix: get-deps exits non-0 code if error occurs

--- dist/get-deps-common.sh
+++ dist/get-deps-common.sh
@@ -4,15 +4,23 @@
 ## Escape consts
 RESET="\033[m"
 YELLOW="\033[33m"
+BRIGHT_RED="\033[91m"
 BRIGHT_GREEN="\033[92m"
 BRIGHT_WHITE="\033[97m"
 
 
-## Print script log message as white text
+## Print script log message as green text
 # Args:
 # $1: Message to print
 function log() {
   echo -e "${BRIGHT_GREEN}${1}${RESET}"
+}
+
+## Print error log message as red text
+# Args:
+# $1: Error message to print
+function error() {
+  echo -e "${BRIGHT_RED}${1}${RESET}"
 }
 
 ## Print packages installation separator
