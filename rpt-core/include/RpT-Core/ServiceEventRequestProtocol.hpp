a03776ec1ec25c24e2728d91846c48359dbc7fde
ThisALV <lel.charriere@orange.fr>
2021-02-21 10:05:23 +0100

    Refactor: ServicesEventRequestProtocol -> ServiceEventRequestProtocol

---
+++ rpt-core/include/RpT-Core/ServiceEventRequestProtocol.hpp
@@ -0,0 +1,11 @@
+#ifndef RPTOGETHER_SERVER_SERVICEEVENTREQUESTPROTOCOL_HPP
+#define RPTOGETHER_SERVER_SERVICEEVENTREQUESTPROTOCOL_HPP
+
+/**
+ * @brief Communication protocol for Event/Request based services.
+ */
+class ServiceEventRequestProtocol {
+
+};
+
+#endif // RPTOGETHER_SERVER_SERVICEEVENTREQUESTPROTOCOL_HPP
