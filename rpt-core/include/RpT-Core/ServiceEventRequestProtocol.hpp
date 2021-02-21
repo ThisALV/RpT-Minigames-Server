98fe3593088d46c14e835239a8e8915f2f5bd2be
ThisALV <lel.charriere@orange.fr>
2021-02-22 00:15:18 +0100

    Implements ServiceEventRequestProtocol

--- rpt-core/include/RpT-Core/ServiceEventRequestProtocol.hpp
+++ rpt-core/include/RpT-Core/ServiceEventRequestProtocol.hpp
@@ -3,7 +3,9 @@
 
 #include <functional>
 #include <initializer_list>
+#include <optional>
 #include <queue>
+#include <stdexcept>
 #include <string>
 #include <string_view>
 #include <unordered_map>
@@ -34,14 +36,71 @@ public:
     virtual std::string_view name() const = 0;
 
     /**
-     * @brief Tries to handle a given Service Request command executed for a given actor
+     * @brief Tries to handle a given command executed for a given actor
      *
      * @param actor Actor who's trying to execute the given SR command
-     * @param sr_command Service Request command to handle
+     * @param sr_command_data Command to handle
      *
      * @returns Boolean which indicates if SR command was successfully handled
      */
-    virtual bool handleRequestCommand(std::string_view actor, std::string_view sr_command) = 0;
+    virtual bool handleRequestCommand(std::string_view actor, std::string_view sr_command_data) = 0;
+};
+
+/**
+ * @brief Thrown by `ServiceEventRequestProtocol` constructor if trying to register already registered service name
+ *
+ * @see ServiceEventRequestProtocol
+ *
+ * @author ThisALV, https://github.com/ThisALV
+ */
+class ServiceNameAlreadyRegistered : public std::logic_error {
+public:
+    /**
+     * @brief Constructs exception with error message including already registered name
+     *
+     * @param name Name used for trying to register service
+     */
+    explicit ServiceNameAlreadyRegistered(const std::string_view name) :
+        std::logic_error { "Service with name \"" + std::string { name } + "\" is already registered" } {}
+};
+
+/**
+ * @brief Thrown by `ServiceEventRequestProtocol::handleServiceRequest()` if trying to execute SR command for
+ * unregistered service
+ *
+ * @see ServiceEventRequestProtocol
+ *
+ * @author ThisALV, https://github.com/ThisALV
+ */
+class ServiceNotFound : public std::logic_error {
+public:
+    /**
+     * @brief Constructs exception with error message including unregistered service name
+     *
+     * @param name Name used for trying to access service
+     */
+    explicit ServiceNotFound(const std::string_view name) :
+            std::logic_error { "Service with name \"" + std::string { name } + "\" not found" } {}
+};
+
+/**
+ * @brief Thrown by `ServiceEventRequestProtocol::handleServiceRequest()` if trying to executor SR command using bad
+ * format
+ *
+ * @see ServiceEventRequestProtocol
+ *
+ * @author ThisALV, https://github.com/ThisALV
+ */
+class BadServiceRequest : public std::logic_error {
+public:
+    /**
+     * @brief Constructs exception with error message including ill formed SR command and bad format reason
+     *
+     * @param sr_command SR command that wasn't executed
+     * @param reason Reason why SR command is ill formed
+     */
+    explicit BadServiceRequest(const std::string_view sr_command, const std::string& reason) :
+            std::logic_error { "SR command \"" + std::string { sr_command } + "\" ill formed: " + reason } {}
 };
 
 /**
@@ -49,7 +108,8 @@ public:
  *
  * Runs a list of named services, learn more about services at `Service` class doc.
  *
- * Each service can receives request from actors and emits event to actors.
+ * Each service can receives request from actors and emits event to actors. Service Request (SR) and Service Event
+ * (SE) both contain command data which describe what action is performed by service.
  *
  * A request is an action that any actor want to perform with a service. The service request command is handled
  * into protocol and the service tries ot handle it. If it was successfully handled, service sends response
@@ -62,14 +122,30 @@ public:
  *
  * SER Protocol:
  *
- * - Service Request command (SR) : `REQUEST <RUID> <SERVICE_NAME> <data>`
+ * - Service Request command (SR) : `REQUEST <RUID> <SERVICE_NAME> <command_data>`
  * - Service Request Response (SRR) : `RESPONSE <RUID> <OK>` or `RESPONSE <RUID> <KO> <ERR_MSG>`
- * - Service Event command (SE) : `EVENT <SERVICE_NAME> <data>`
+ * - Service Request Dispatch (SRD) : `REQUEST_FROM <actor> <SERVICE_NAME> <command_data>`
+ * - Service Event command (SE) : `EVENT <SERVICE_NAME> <command_data>`
  *
  * @author ThisALV, https://github.com/ThisALV
  */
 class ServiceEventRequestProtocol {
 private:
+    // Prefix for Service Request (SR) commands
+    static constexpr std::string_view REQUEST_PREFIX { "REQUEST" };
+    // Prefix for Service Request Response (SRR) commands
+    static constexpr std::string_view RESPONSE_PREFIX { "RESPONSE" };
+    // Prefix for Service Event (SE) commands
+    static constexpr std::string_view EVENT_PREFIX { "EVENT" };
+
+    /**
+     * @brief Get list of words inside given Service Request command, separated by char ' '
+     *
+     * @param sr_command Service Request command to parse
+     * @returns String views to each separated word contained inside `sr_command`
+     */
+    static std::vector<std::string_view> getWordsFor(std::string_view sr_command);
+
     std::unordered_map<std::string_view, std::reference_wrapper<Service>> running_services_;
     std::queue<std::string> services_events_queue_;
 
@@ -79,21 +155,42 @@ public:
      *
      * Each service will be named from its `Service::name()` returned value.
      *
+     * @throws ServiceNameAlreadyRegistered if a service name appears twice into services list
+     *
      * @param services References to services
      */
     ServiceEventRequestProtocol(const std::initializer_list<std::reference_wrapper<Service>>& services);
 
     /**
+     * @brief Get if given service is already registered
+     *
+     * @param service Name of service to check for
+     *
+     * @returns If registered services contains a service with given name
+     */
+    bool isRegistered(std::string_view service) const;
+
+    /**
      * @brief Try to treat the given Service Request command
      *
-     * Find an appropriate service, and make it handle the given SR command with actor executor.
+     * Find appropriate service, and make it handle the given SR command with actor executor.
      *
      * @param actor Actor who's trying to execute that SR command
      * @param sr_command Service Request command to handle
      *
-     * @returns Boolean indicates if service was found and if SR command was successfully handled
+     * @throws BadServiceRequest if service_request is ill formed
+     * @throws ServiceNotFound if service into service_request isn't registered
+     *
+     * @returns Optional value, initialized with error message if not handled successfully, uninitialized else
      */
     bool handleServiceRequest(std::string_view actor, std::string_view service_request);
+
+    /**
+     * @brief Push Service Event command into events queue
+     *
+     * @param se_command SE command to push
+     */
+    void pushServiceEvent(std::string se_command);
 
     /**
      * @brief Poll next Service Event command in queue, do nothing if queue is empty
--- rpt-core/src/ServiceEventRequestProtocol.cpp
+++ rpt-core/src/ServiceEventRequestProtocol.cpp
@@ -1,13 +1,107 @@
 #include <RpT-Core/ServiceEventRequestProtocol.hpp>
 
+#include <algorithm>
+#include <cassert>
+
 
 namespace RpT::Core {
 
 
+std::vector<std::string_view> ServiceEventRequestProtocol::getWordsFor(std::string_view sr_command) {
+    std::vector<std::string_view> parsed_words;
+
+    // Begin and end constant iterators for SR command string
+    const auto cmd_begin { sr_command.cbegin() };
+    const auto cmd_end { sr_command.cend() };
+
+    // Starts without any word parsed yet
+    std::vector<std::string_view> words;
+
+    // Starts string parsing from the beginning
+    auto word_begin { cmd_begin };
+    auto word_end { cmd_begin };
+
+    // Until end of string is reached
+    while (word_end != cmd_end) {
+        word_end = std::find(word_begin + 1, cmd_end, ' '); // Find next ' ' char into unparsed command string
+
+        // Word begin and end index calculations
+        const long word_begin_i { word_begin - cmd_begin };
+        const long word_end_i { word_end - cmd_end };
+
+        // Indexes must be positives, else it means either word_begin or word_end is out of range
+        assert(word_begin_i >= 0 && word_end_i >= 0);
+
+        // Adds parsed word from its first char to next space excluded
+        words.push_back(sr_command.substr(word_begin_i, word_end_i));
+
+        word_begin = word_end ; // Next search should begin after current word
+    }
+
+    return words;
+}
+
 ServiceEventRequestProtocol::ServiceEventRequestProtocol(
         const std::initializer_list<std::reference_wrapper<Service>>& services) {
 
+    // Each given service reference must be registered as running service
+    for (const auto service_ref : services) {
+        const std::string_view service_name { service_ref.get().name() };
 
+        if (isRegistered(service_name)) // Service name must be unique among running services
+            throw ServiceNameAlreadyRegistered { service_ref.get().name() };
+
+        const auto service_registration_result { running_services_.insert({ service_name, service_ref }) };
+        // Must be sure that service has been successfully registered, this is why insertion result is saved
+        assert(service_registration_result.second);
+    }
 }
+
+bool ServiceEventRequestProtocol::isRegistered(const std::string_view service) const {
+    return running_services_.count(service) == 1; // Returns if service name is present among running services
+}
+
+bool ServiceEventRequestProtocol::handleServiceRequest(const std::string_view actor,
+                                                       const std::string_view service_request) {
+
+    // Get all parsed words into SR command
+    const std::vector<std::string_view> sr_command_words { getWordsFor(service_request) };
+
+    // At least two words must be present for SER command prefix and service of request
+    if (sr_command_words.size() < 2)
+        throw BadServiceRequest { service_request, "SER command prefix and intended service must be given" };
+
+    // First word must corresponds to SR command prefix
+    if (sr_command_words.at(0) != REQUEST_PREFIX)
+        throw BadServiceRequest { service_request, "SER command prefix must be REQUEST" };
+
+    const std::string_view intended_service_name { sr_command_words.at(1) }; // Retrieve service name from 2nd word
+
+    // Service must be registered
+    if (!isRegistered(intended_service_name))
+        throw ServiceNotFound { intended_service_name };
+
+    Service& intended_service { running_services_.at(intended_service_name).get() };
+
+    // Execute given command, and returns service execution result
+    return intended_service.handleRequestCommand(actor, service_request);
+}
+
+void ServiceEventRequestProtocol::pushServiceEvent(std::string se_command) {
+    services_events_queue_.push(std::move(se_command)); // Given SE command is moved to queue
+}
+
+std::optional<std::string> ServiceEventRequestProtocol::pollServiceEvent() {
+    // Event to poll is uninitialized
+    std::optional<std::string> next_event;
+
+    if (!services_events_queue_.empty()) { // If there is at least one event to poll
+        next_event = std::move(services_events_queue_.front()); // Then event is moved to polled event
+        services_events_queue_.pop(); // And moved event is popped from queue
+    }
+
+    return next_event;
+}
+
 
 }
