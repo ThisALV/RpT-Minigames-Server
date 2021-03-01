4cac4f4f811e3e81c118cceb39b09f672d27ecca
ThisALV <lel.charriere@orange.fr>
2021-03-01 01:01:40 +0100

    Adds LoggingContext unit testing

---
+++ rpt-tests/src/LoggingContextTests.cpp
@@ -0,0 +1,167 @@
+#include <RpT-Testing/TestingUtils.hpp>
+
+#include <RpT-Utils/LoggingContext.hpp>
+#include <RpT-Utils/LoggerView.hpp>
+
+
+using namespace RpT::Utils;
+
+
+BOOST_AUTO_TEST_SUITE(LoggingContextTests)
+
+/*
+ * Constructor tests
+ */
+
+BOOST_AUTO_TEST_SUITE(Constructor)
+
+BOOST_AUTO_TEST_CASE(DefaultLoggingLevel) {
+    const LoggingContext logging_context;
+
+    // Default logging lvl should be INFO
+    BOOST_CHECK_EQUAL(logging_context.retrieveLoggingLevel(), LogLevel::INFO);
+}
+
+BOOST_AUTO_TEST_CASE(FatalLoggingLevel) {
+    const LoggingContext logging_context { LogLevel::FATAL };
+
+    BOOST_CHECK_EQUAL(logging_context.retrieveLoggingLevel(), LogLevel::FATAL);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+/*
+ * newLoggerFor() tests
+ */
+
+BOOST_AUTO_TEST_SUITE(NewLoggerFor)
+
+BOOST_AUTO_TEST_CASE(OneRegisteredLogger) {
+    LoggingContext logging_context;
+
+    // Registering new loggers
+    const LoggerView logger_a { "LoggerA", logging_context };
+
+    // Should be equals, as generic name (or general purpose) is "LoggerA" and UID should be 0
+    BOOST_CHECK_EQUAL(logger_a.name(), "LoggerA-0");
+}
+
+BOOST_AUTO_TEST_CASE(ManyRegisteredLoggersWithSamePurpose) {
+    LoggingContext logging_context;
+
+    // Registering new loggers
+    const LoggerView logger_a0 { "LoggerA", logging_context };
+    const LoggerView logger_a1 { "LoggerA", logging_context };
+    const LoggerView logger_a2 { "LoggerA", logging_context };
+
+    // Should be equals, as generic name is "LoggerA" and UID increments by 1 for each logger registered with same name
+    BOOST_CHECK_EQUAL(logger_a0.name(), "LoggerA-0");
+    BOOST_CHECK_EQUAL(logger_a1.name(), "LoggerA-1");
+    BOOST_CHECK_EQUAL(logger_a2.name(), "LoggerA-2");
+}
+
+BOOST_AUTO_TEST_CASE(ManyRegisteredLoggersWithDifferentPurposes) {
+    LoggingContext logging_context;
+
+    // Registering new loggers
+    const LoggerView logger_a { "LoggerA", logging_context };
+    const LoggerView logger_b { "LoggerB", logging_context };
+    const LoggerView logger_c { "LoggerC", logging_context };
+
+    // Should be equals, as generic names are "LoggerA...B...C" and are always different (so UID isn't incremented)
+    BOOST_CHECK_EQUAL(logger_a.name(), "LoggerA-0");
+    BOOST_CHECK_EQUAL(logger_b.name(), "LoggerB-0");
+    BOOST_CHECK_EQUAL(logger_c.name(), "LoggerC-0");
+}
+
+BOOST_AUTO_TEST_CASE(ManyRegiteredLoggersWithDifferentAndSamePurposes) {
+    LoggingContext logging_context;
+
+    /*
+     * Registering new loggers
+     */
+
+    const LoggerView logger_a0 { "LoggerA", logging_context };
+    const LoggerView logger_a1 { "LoggerA", logging_context };
+    const LoggerView logger_a2 { "LoggerA", logging_context };
+
+    const LoggerView logger_b0 { "LoggerB", logging_context };
+    const LoggerView logger_b1 { "LoggerB", logging_context };
+
+    const LoggerView logger_c0 { "LoggerC", logging_context };
+    const LoggerView logger_c1 { "LoggerC", logging_context };
+    const LoggerView logger_c2 { "LoggerC", logging_context };
+    const LoggerView logger_c3 { "LoggerC", logging_context };
+
+    // Should be equals, as generic name is "LoggerA" and UID increments by 1 for each logger registered with same name
+    BOOST_CHECK_EQUAL(logger_a0.name(), "LoggerA-0");
+    BOOST_CHECK_EQUAL(logger_a1.name(), "LoggerA-1");
+    BOOST_CHECK_EQUAL(logger_a2.name(), "LoggerA-2");
+
+    // Should be equals, as generic name is "LoggerB" and UID increments by 1 for each logger registered with same name
+    BOOST_CHECK_EQUAL(logger_b0.name(), "LoggerB-0");
+    BOOST_CHECK_EQUAL(logger_b1.name(), "LoggerB-1");
+
+    // Should be equals, as generic name is "LoggerC" and UID increments by 1 for each logger registered with same name
+    BOOST_CHECK_EQUAL(logger_c0.name(), "LoggerC-0");
+    BOOST_CHECK_EQUAL(logger_c1.name(), "LoggerC-1");
+    BOOST_CHECK_EQUAL(logger_c2.name(), "LoggerC-2");
+    BOOST_CHECK_EQUAL(logger_c3.name(), "LoggerC-3");
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+/**
+ * updateLoggingLevel() tests
+ */
+
+BOOST_AUTO_TEST_SUITE(UpdateLoggingLevel)
+
+BOOST_AUTO_TEST_CASE(LoggersRegisteredAfter) {
+    LoggingContext logging_context;
+
+    logging_context.updateLoggingLevel(LogLevel::WARN); // Updating current logging level
+    // Registering new loggers into context after update
+    const LoggerView logger_a { "LoggerA", logging_context };
+    const LoggerView logger_b { "LoggerB", logging_context };
+
+    // Logging level should be set to WARN for all running loggers inside context
+    BOOST_CHECK_EQUAL(logger_a.loggingLevel(), LogLevel::WARN);
+    BOOST_CHECK_EQUAL(logger_b.loggingLevel(), LogLevel::WARN);
+}
+
+BOOST_AUTO_TEST_CASE(LoggersRegisteredBefore) {
+    LoggingContext logging_context;
+
+    // Registering new loggers into context before update
+    const LoggerView logger_a { "LoggerA", logging_context };
+    const LoggerView logger_b { "LoggerB", logging_context };
+    logging_context.updateLoggingLevel(LogLevel::WARN); // Updating current logging level
+
+    // Logging level should be set to WARN for all running loggers inside context
+    BOOST_CHECK_EQUAL(logger_a.loggingLevel(), LogLevel::WARN);
+    BOOST_CHECK_EQUAL(logger_b.loggingLevel(), LogLevel::WARN);
+}
+
+BOOST_AUTO_TEST_CASE(LoggersRegisteredBeforeAndAfter) {
+    LoggingContext logging_context;
+
+    // Registering new loggers into context before update
+    const LoggerView logger_a { "LoggerA", logging_context };
+    const LoggerView logger_b { "LoggerB", logging_context };
+    // Updating current logging level
+    logging_context.updateLoggingLevel(LogLevel::WARN);
+    // Registering new loggers into context after update
+    const LoggerView logger_c { "LoggerC", logging_context };
+    const LoggerView logger_d { "LoggerD", logging_context };
+
+    // Logging level should be set to WARN for all running loggers inside context
+    BOOST_CHECK_EQUAL(logger_a.loggingLevel(), LogLevel::WARN);
+    BOOST_CHECK_EQUAL(logger_b.loggingLevel(), LogLevel::WARN);
+    BOOST_CHECK_EQUAL(logger_c.loggingLevel(), LogLevel::WARN);
+    BOOST_CHECK_EQUAL(logger_d.loggingLevel(), LogLevel::WARN);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE_END()
