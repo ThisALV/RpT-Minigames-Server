d53e462ba686a8ce69739f66d546122dd1e270e0
ThisALV <lel.charriere@orange.fr>
2021-03-04 23:42:04 +0100

    Implements NetworkBackend events queue and separate handshake handling

--- rpt-network/include/RpT-Network/NetworkBackend.hpp
+++ rpt-network/include/RpT-Network/NetworkBackend.hpp
@@ -2,6 +2,8 @@
 #define RPTOGETHER_SERVER_NETWORKBACKEND_HPP
 
 #include <cstdint>
+#include <optional>
+#include <queue>
 #include <stdexcept>
 #include <string>
 #include <unordered_map>
@@ -16,7 +18,7 @@ namespace RpT::Network {
 
 
 /**
- * @brief Thrown by `NetworkBackend::handleClientMessage()` if received client RPTL message is ill-formed.
+ * @brief Thrown by `NetworkBackend::handleMessage()` if received client RPTL message is ill-formed.
  *
  * @author ThisALV, https://github.com/ThisALV
  */
@@ -37,40 +39,82 @@ public:
  * Implements a common protocol (RPTL, or RpT Login Protocol) which is managing who is connected or disconnected, and
  * which is associating player informations (like name) with corresponding actor UID.
  *
+ * RPTL protocol can receive handshake from unregistered client connections to register them. When a client is
+ * registered, it can send messages to server, which are following format: `<RPTL_command> <args>...`
+ *
  * RPTL Protocol (after connection began):
- * - Log in: `LOGIN <name>`
- * - Log out (clean way): `LOGOUT`
- * - Send Service Request command: if message doesn't begin with `LOGIN` or `LOGOUT`, it's parsed as SR command and
- * will be given to `RpT::Core::ServiceEventRequestProtocol` using `RpT::Core::ServiceRequestEvent`.
+ * - Handshake: `LOGIN <name>`, must NOT be registered
+ * - Log out (clean way): `LOGOUT`, must BE registered
+ * - Send Service Request command: `SERVICE <SR_command>` (see `RpT::Core::ServiceEventRequestProtocol`), must BE
+ * registered
+ *
+ * An events queue is internally used for handling case of many input events triggered between `waitForInput()` calls.
  *
  * @author ThisALV, https://github.com/ThisALV
  */
 class NetworkBackend : public Core::InputOutputInterface {
 private:
-    std::uint64_t uid_count_;
-    std::unordered_map<std::uint64_t, std::string> logged_in_actors_;
-
     /*
      * Prefixes for RPTL protocol commands invoked by clients
      */
 
-    static constexpr std::string_view LOGIN_COMMAND { "LOGIN" };
+    static constexpr std::string_view HANDSHAKE_COMMAND { "LOGIN" };
     static constexpr std::string_view LOGOUT_COMMAND { "LOGOUT" };
+    static constexpr std::string_view SERVICE_COMMAND { "SERVICE" };
+
+    std::uint64_t uid_count_;
+    std::unordered_map<std::uint64_t, std::string> logged_in_actors_;
+    std::queue<Core::AnyInputEvent> input_events_queue_;
 
 protected:
     /**
-     * @brief Handles given received RPTL message from client with associated actor UID and retrieves triggered input
-     * event
+     * @brief Parses received handshake and registers new actor
+     *
+     * @param client_handshake Handshake data received from connected client
+     *
+     * @returns Input triggered by handshake, means that it was handled successfully, and actor has been registered
+     */
+    Core::JoinedEvent handleHandshake(const std::string& client_handshake);
+
+    /**
+     * @brief Handles given received RPTL message from client with associated registered actor UID and retrieves
+     * triggered input event.
      *
      * @param client_actor UID for actor representing this client
      * @param client_message Received client message (received network data) to handle
      *
      * @returns Event triggered by message, must be `Core::JoinedEvent`, `Core::LeftEvent` or
-     * `Core::ServiceRequestEvent`, as only these events can be triggered by client
+     * `Core::ServiceRequestEvent`, as only these events can be triggered by a registered actor
      *
-     * @throws BadClientMessage if given client message is ill-formed (missing args)
+     * @throws BadClientMessage if given client message is ill-formed (missing args, unknown command...)
      */
-    Core::AnyInputEvent handleClientMessage(std::uint64_t client_actor, const std::string& client_message);
+    Core::AnyInputEvent handleMessage(std::uint64_t client_actor, const std::string& client_message);
+
+    /**
+     * @brief Push given triggered input event into queue
+     *
+     * @param input_event Triggered input event to push
+     */
+    void pushInputEvent(Core::AnyInputEvent input_event);
+
+    /**
+     * @brief If input events queue isn't empty, take and retrive next event to handle
+     *
+     * Called at `waitForInput()` beginning to poll any queued event before waiting for another to be triggered.
+     *
+     * @returns Next triggered input event if any, uninitialized otherwise
+     */
+    std::optional<Core::AnyInputEvent> pollInputEvent();
+
+    /**
+     * @brief Wait for external input event to happen
+     *
+     * Called by `waitForInput()` when events queue is empty implementation, must be overridden by `NetworkBackend`
+     * implementations, but not called.
+     *
+     * @returns Input event triggered by external cause
+     */
+    virtual Core::AnyInputEvent waitForEvent() = 0;
 };
 
 
