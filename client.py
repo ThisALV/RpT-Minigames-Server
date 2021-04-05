5fad3e9fafa90719c79c4f4419d8a91602d60702
ThisALV <lel.charriere@orange.fr>
2021-04-05 22:09:21 +0200

    Fix: receiving messages works

--- client.py
+++ client.py
@@ -1,7 +1,6 @@
 import asyncio
 import signal
 import ssl
-import threading
 import websockets
 from sys import argv
 
@@ -14,16 +13,17 @@ tls_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
 tls_context.keylog_filename = "keylog.pem"  # Wireshark will need that for debugging
 tls_context.load_verify_locations(certificate)  # localhost requires self-signed PEM cert
 
-console_lock = asyncio.Lock()  # To not write a received message and read a sending message simultaneously
-input_required = threading.Event()  # To notify the task sending message that it should prompt for message into stdin
+
+def require_input(input_required: asyncio.Event):
+    input_required.set()
 
 
-async def send_messages(connection: websockets.WebSocketClientProtocol):
+async def send_messages(connection: websockets.WebSocketClientProtocol, console_lock: asyncio.Lock, input_required: asyncio.Event):
     """Wait for the console to be available then read input message from stdin and send it to server."""
 
     while connection.open:  # Waits for next Ctrl+C until connection has been closed
         try:
-            input_required.wait()  # Waits for Ctrl+C
+            await input_required.wait()  # Waits for Ctrl+C
 
             async with console_lock:  # Waits for the console to be available before reading stdin
                 rptl_message = input("Send: ")
@@ -35,7 +35,7 @@ async def send_messages(connection: websockets.WebSocketClientProtocol):
             print(f"Connection was closed: {closed.reason}")
 
 
-async def receive_messages(connection: websockets.WebSocketClientProtocol):
+async def receive_messages(connection: websockets.WebSocketClientProtocol, console_lock: asyncio.Lock):
     """Listen for next RPTL message and wait for the console to be available then displays it to stdout."""
 
     while connection.open:  # Waits for next RPTL message until connection has been closed
@@ -52,8 +52,12 @@ async def rptl_connection(connection: websockets.WebSocketClientProtocol):
     """Listen for RPTL message on given connection and displays them to stdout.
     At SIGINT, messages no longer displayed until RPTL message has been written to stdin and sent to server."""
 
-    receiving = asyncio.create_task(receive_messages(connection))
-    sending = asyncio.create_task(send_messages(connection))
+    console_lock = asyncio.Lock()  # To not write a received message and read a sending message simultaneously
+    input_required = asyncio.Event()  # To notify the task sending message that it should prompt for message into stdin
+
+    asyncio.get_running_loop().add_signal_handler(signal.SIGINT, require_input, input_required)  # When Ctrl+C is triggered, prompt for RPTL message
+    receiving = asyncio.create_task(receive_messages(connection, console_lock))
+    sending = asyncio.create_task(send_messages(connection, console_lock, input_required))
 
     await asyncio.gather(receiving, sending)
 
@@ -67,5 +71,5 @@ async def websocket_connection(host_url: str, security_context: ssl.SSLContext):
     # Once connection is made, starts RPTL protocol until disconnection
     await rptl_connection(connection)
 
-signal.signal(signal.SIGINT, lambda _, __: input_required.set())  # When Ctrl+C is triggered, prompt for RPTL message
+
 asyncio.run(websocket_connection(url, tls_context))
