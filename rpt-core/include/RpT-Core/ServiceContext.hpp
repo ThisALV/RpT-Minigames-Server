6b0e3ce142e68ffbe1b50e3317d3a828492207bf
ThisALV <lel.charriere@orange.fr>
2021-04-22 15:42:38 +0200

    ServiceContext provides timers token

--- rpt-core/include/RpT-Core/ServiceContext.hpp
+++ rpt-core/include/RpT-Core/ServiceContext.hpp
@@ -22,21 +22,31 @@ namespace RpT::Core {
 class ServiceContext {
 private:
     std::size_t events_count_;
+    std::size_t timers_count_;
 
 public:
     /**
-     * @brief Initialize events count at 0
+     * @brief Initialize events count at 0, same for timers count
      */
     ServiceContext();
 
     /**
-     * @brief Increments events count and retrieve its previous value
+     * @brief Increments events count and retrieves its previous value
      *
      * @note Called by `Service::emitEvent()` for retrieving triggered event ID, shouldn't be called by user.
      *
      * @return Previous value for events count
      */
     std::size_t newEventPushed();
+
+    /**
+     * @brief Increments timers count and retrieves its previous value
+     *
+     * @note Called by `Timer` constructor to determine instance token, shouldn't be called by user
+     *
+     * @returns A free `Timer` token to use
+     */
+    std::size_t newTimerCreated();
 };
 
 
--- rpt-core/src/ServiceContext.cpp
+++ rpt-core/src/ServiceContext.cpp
@@ -4,10 +4,14 @@
 namespace RpT::Core {
 
 
-ServiceContext::ServiceContext() : events_count_ { 0 } {}
+ServiceContext::ServiceContext() : events_count_ { 0 }, timers_count_ { 0 } {}
 
 std::size_t ServiceContext::newEventPushed() {
     return events_count_++;
+}
+
+std::size_t ServiceContext::newTimerCreated() {
+    return timers_count_++;
 }
 
 
