083225027ae641489e0b31792c46f6bd56aee1b9
ThisALV <lel.charriere@orange.fr>
2021-06-26 14:19:19 +0200

    Removes useless overloaded operator== as value semantic includes it

--- rpt-tests/include/RpT-Testing/SerTestingUtils.hpp
+++ rpt-tests/include/RpT-Testing/SerTestingUtils.hpp
@@ -8,17 +8,6 @@ namespace RpT::Core {
 
 
 /**
- * As `ServiceEvent` is an Entity Class, testing equality with it only make sense on a testing content to checks for
- * its field values
- *
- * @param lhs Left-Hand side event
- * @param rhs Right-Hand side event
- *
- * @returns `true` if command and optional UIDs list are equals for both `lhs` and `rhs`, `false` otherwise
- */
-bool operator==(const ServiceEvent& lhs, const ServiceEvent& rhs);
-
-/**
  * Prints event command data in quotes then a comma-separated list of actor UIDs
  *
  * @param out Stream to be written
